c7b5117db39b960a409cbf2958f21033179f5dfc
Brandon🌩️H <bc3tech@users.noreply.github.com>
2026-04-13 09:04:41 -0700

    Squashed commit of the following:
    commit 88f537a6353e0994bf91c943780b622cbad05e7f
    Author: Brandon🌩️H <bc3tech@users.noreply.github.com>
    Date:   Mon Apr 13 09:00:56 2026 -0700
    
        Moving MeterExtensions down into the library

---
+++ AgentFramework.OpenTelemetry/MeterExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace AgentFramework.OpenTelemetry;
+
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+
+public static class MeterExtensions
+{
+    public static void LogMetric<T>(this Meter meter, string name, T value, Dictionary<string, object?> tags) where T : struct => meter.LogMetric(name, value, tags.ToArray());
+
+    public static void LogMetric<T>(this Meter meter, string name, T value, params KeyValuePair<string, object?>[] tags) where T : struct => meter.CreateHistogram<T>(name).Record(value, tags);
+
+    public static void LogMetric<T>(this Histogram<T> meter, T value, params KeyValuePair<string, object?>[] tags) where T : struct => meter.Record(value, tags);
+
+    public static void LogMetric<T>(this Counter<T> meter, T value, params KeyValuePair<string, object?>[] tags) where T : struct => meter.Add(value, tags);
+}
